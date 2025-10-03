bffbe543bce9ddbc79dc916367d8a1a3ce08b462
M-Mabrouk <m.a.mabrouk@outlook.com>
2025-10-03 21:03:15 +0300

    fixed: typo in import crashed frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
 import './styles.css'
 import App from './App.jsx'
-import { AuthProvider } from './context/authContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
